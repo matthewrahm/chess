4825ff2bfa56170ff23e5226d051a76b9891ee59
matthewrahm <matthewdrahm@gmail.com>
2026-03-16 14:23:34 -0600

    Implement MySqlUserDAO methods for createUser, getUser, and clear

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -12,17 +12,44 @@ public class MySqlUserDAO implements UserDAO {
 
     @Override
     public void clear() throws DataAccessException {
-        throw new DataAccessException("not implemented");
+        var sql = "TRUNCATE TABLE users";
+        try (var conn = DatabaseManager.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+            stmt.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to clear users", ex);
+        }
     }
 
     @Override
     public void createUser(UserData user) throws DataAccessException {
-        throw new DataAccessException("not implemented");
+        var sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+        try (var conn = DatabaseManager.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, user.username());
+            stmt.setString(2, user.password());
+            stmt.setString(3, user.email());
+            stmt.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to create user", ex);
+        }
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
-        throw new DataAccessException("not implemented");
+        var sql = "SELECT username, password, email FROM users WHERE username=?";
+        try (var conn = DatabaseManager.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, username);
+            try (var rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return new UserData(rs.getString("username"), rs.getString("password"), rs.getString("email"));
+                }
+                return null;
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to get user", ex);
+        }
     }
 
     private void configureDatabase() throws DataAccessException {
