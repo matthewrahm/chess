dd074b7743e817cebe5d8b151fdeea6fcc276b54
matthewrahm <matthewdrahm@gmail.com>
2026-03-22 10:19:47 -0600

    Wire REPL into ClientMain with configurable server port

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,8 @@
 package client;
 
-import chess.*;
-
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        int port = (args.length > 0) ? Integer.parseInt(args[0]) : 8080;
+        new Repl(port).run();
     }
 }
