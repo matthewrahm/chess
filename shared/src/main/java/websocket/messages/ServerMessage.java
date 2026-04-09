af8b2e4f978dc77d31f24b6e254199fdfd3a449f
matthewrahm <matthewdrahm@gmail.com>
2026-04-09 12:03:11 -0600

    Add WebSocket server message classes to shared module
    Add ServerMessage base class and LoadGameMessage, ErrorMessage,
    NotificationMessage subclasses for server-to-client communication.

---
+++ shared/src/main/java/websocket/messages/ServerMessage.java
@@ -0,0 +1,41 @@
+package websocket.messages;
+
+import java.util.Objects;
+
+/**
+ * Represents a message the server can send through a WebSocket
+ */
+public class ServerMessage {
+
+    ServerMessageType serverMessageType;
+
+    public enum ServerMessageType {
+        LOAD_GAME,
+        ERROR,
+        NOTIFICATION
+    }
+
+    public ServerMessage(ServerMessageType type) {
+        this.serverMessageType = type;
+    }
+
+    public ServerMessageType getServerMessageType() {
+        return this.serverMessageType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ServerMessage that)) {
+            return false;
+        }
+        return getServerMessageType() == that.getServerMessageType();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getServerMessageType());
+    }
+}
